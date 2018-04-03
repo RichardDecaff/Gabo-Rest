28e2b777f25a4e64d8b0e3bd9543fb92d037ee51
Gabriel <gabso94@gmail.com>
2018-04-02 19:13:53 -0500

    rutas estaticas

--- controller/VistasController.js
+++ controller/VistasController.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+router.use(express.static('./views'));
+
 router.route('/login').get(function(req, res) {
   fs.readFile('./views/login.html', 'utf-8', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
